26018b260beaaddeb2230e31edec4d252b9700f6
toi.nguyen@sotatek.com <toinv@123>
2020-12-14 10:15:54 +0700

    sua giao dien 4

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -15,10 +15,10 @@
     <div class="container">
         <!-- row -->
         <div class="row">
-            <div class="col-md-3" style="border-right: 3px solid #fff">
+            <div class="col-md-2" style="border-right: 3px solid #fff">
                 <a href="index.html" class="logo pull-right"><img width="150" src="{{asset('./img/logo-viet-farm-footer.jpg')}}" alt=""></a>
             </div>
-            <div class="col-md-5" style="border-right: 3px solid #fff">
+            <div class="col-md-4" style="border-right: 3px solid #fff">
                 <div class="footer-widget">
                 <h3 class="footer-title">LIÊN HỆ</h3>
                 <p><i class="fa fa-home"></i>&emsp; 76 Phùng Khoan, Thanh Xuân, Hà Nội</p>
@@ -26,7 +26,7 @@
                 <p><i class="fa fa-envelope"></i>&emsp; bacdaibang@gmail.com</p>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="footer-widget">
                     <h3 class="footer-title">NHẬN THÔNG BÁO</h3>
                     <div class="newsletter-widget">
@@ -35,6 +35,16 @@
                             <button class="btn btn-danger left" style="margin-top: 20px">Gửi</button>
                         </form>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="footer-widget">
+                    <h3 class="footer-title">LIÊN HỆ</h3>
+                    <iframe width="100%" height="auto" id="gmap_canvas" src="https://maps.google.com/maps?q=76%20Ph%C3%B9ng%20Khoan%2C%20Thanh%20Xu%C3%A2n%2C%20H%C3%A0%20N%E1%BB%99i%2C%20Vi%E1%BB%87t%20Nam&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+
+                    </iframe>
+                    <a href="https://embedgooglemap.net/mapv2/"></a>
+                    <style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
                 </div>
             </div>
         </div>
