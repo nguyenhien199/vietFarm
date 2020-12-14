7f6610367949cc38eaf3b8df07772c89e68167c7
romecoder@gmail.com <tung12345>
2020-12-14 21:54:56 +0700

    sua doi tac

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -4,25 +4,25 @@
     <!-- SECTION -->
     <div class="section" style="padding-top:0 ">
         <!-- container -->
-            <!-- row -->
+        <!-- row -->
 
-            <div>
-                <!-- ======= Intro Section ======= -->
-                <div class="intro intro-carousel">
-                    <script>
-                        $(document).ready(function() {
-                            $(".owl-stage").owlCarousel({
-                                loop: true,
-                                autoplay: true,
-                                autoplayTimeout: 5000,
-                                smartSpeed: 1500,
-                                animateIn: 'linear',
-                                animateOut: 'linear'
-                            });
+        <div>
+            <!-- ======= Intro Section ======= -->
+            <div class="intro intro-carousel">
+                <script>
+                    $(document).ready(function() {
+                        $(".owl-stage").owlCarousel({
+                            loop: true,
+                            autoplay: true,
+                            autoplayTimeout: 5000,
+                            smartSpeed: 1500,
+                            animateIn: 'linear',
+                            animateOut: 'linear'
                         });
-                    </script>
-                    <div id="carousel" class="owl-carousel owl-theme">
-                        @foreach($slide as $sl)
+                    });
+                </script>
+                <div id="carousel" class="owl-carousel owl-theme">
+                    @foreach($slide as $sl)
                         <div class="carousel-item-a intro-item bg-image" style="background-image: url({{asset($sl->image)}})">
                             <div class="overlay overlay-a"></div>
                             <div class="intro-content display-table">
@@ -40,40 +40,40 @@
                                 </div>
                             </div>
                         </div>
-                        @endforeach
-                    </div>
-                </div><!-- End Intro Section -->
-            </div>
+                    @endforeach
+                </div>
+            </div><!-- End Intro Section -->
+        </div>
     </div>
     <!-- ======= Testimonials Section ======= -->
     <section class="section-testimonials section-t8 nav-arrow-a">
         <div class="container">
-{{--            <div class="row">--}}
-{{--                <div class="col-md-12">--}}
-{{--                    <div class="title-wrap d-flex justify-content-between">--}}
-{{--                        <div class="title-box">--}}
-{{--                            <h2 class="title-a">Testimonials</h2>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            {{--            <div class="row">--}}
+            {{--                <div class="col-md-12">--}}
+            {{--                    <div class="title-wrap d-flex justify-content-between">--}}
+            {{--                        <div class="title-box">--}}
+            {{--                            <h2 class="title-a">Testimonials</h2>--}}
+            {{--                        </div>--}}
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
             <div id="testimonial-carousel" class="owl-carousel owl-arrow">
                 @foreach($newPost as $new)
-                <div class="carousel-item-a">
-                    <div class="post row">
-                        <div class="col-md-5">
-                            <a class="post-img" href="{{url('bai-viet',[$new->url])}}" style="margin-top: 50px"><img src="{{$new->image}}" alt=""></a>
+                    <div class="carousel-item-a">
+                        <div class="post row">
+                            <div class="col-md-5">
+                                <a class="post-img" href="{{url('bai-viet',[$new->url])}}" style="margin-top: 50px"><img src="{{$new->image}}" alt=""></a>
+                            </div>
+                            <div class="col-md-7" style="margin-top: 10px;">
+                                <h3 class="post-title">
+                                    <a href="{{url('bai-viet',[$new->url])}}">{{$new->title}}</a></h3>
+                                <p>
+                                    {{$new->description}}
+                                </p>
+                                <a class="btn btn-info btn-x-ch-tit" href="{{url('bai-viet',[$new->url])}}">XEM CHI TIẾT</a>
+                            </div>
                         </div>
-                        <div class="col-md-7" style="margin-top: 10px;">
-                            <h3 class="post-title">
-                                <a href="{{url('bai-viet',[$new->url])}}">{{$new->title}}</a></h3>
-                            <p>
-                               {{$new->description}}
-                            </p>
-                            <a class="btn btn-info btn-x-ch-tit" href="{{url('bai-viet',[$new->url])}}">XEM CHI TIẾT</a>
-                </div>
-            </div>
-                </div>
+                    </div>
                 @endforeach
             </div>
         </div>
@@ -87,50 +87,50 @@
                 <hr style="width: 200px; text-align: center; border: 1px solid"/>
             </div>
             <div class="row">
-               <div class="col-md-6">
-                   <div class="info-1">
-                       <img style="height: 50px;" src="{{asset('img/icon-info-1.jpg')}}">
-                       <div style="clear: both">
-                           <h4>Thực phẩm Organic</h4>
-                           <p>
-                               Luông mang trong mình tự hào với những thực
-                               phẩm organic có chất lượng vượt trội khác biệt
-                               hẳn so với những dòng sản phẩm cùng loại trên thị
-                               trường
-                           </p>
-                       </div>
-                   </div>
-               </div>
+                <div class="col-md-6">
+                    <div class="info-1">
+                        <img style="height: 50px;" src="{{asset('img/icon-info-1.jpg')}}">
+                        <div style="clear: both">
+                            <h4>Thực phẩm Organic</h4>
+                            <p>
+                                Luông mang trong mình tự hào với những thực
+                                phẩm organic có chất lượng vượt trội khác biệt
+                                hẳn so với những dòng sản phẩm cùng loại trên thị
+                                trường
+                            </p>
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-6">
                     <div class="info-2 pull-right">
                         <img class="pull-right" style="height: 50px;" src="{{asset('img/icon-info-2.jpg')}}">
-                       <div class="text-right" style="clear: both">
-                           <h4>Thực phẩm Organic</h4>
-                           <p>
-                               Luông mang trong mình tự hào với những thực
-                               phẩm organic có chất lượng vượt trội khác biệt
-                               hẳn so với những dòng sản phẩm cùng loại trên thị
-                               trường
-                           </p>
-                       </div>
+                        <div class="text-right" style="clear: both">
+                            <h4>Thực phẩm Organic</h4>
+                            <p>
+                                Luông mang trong mình tự hào với những thực
+                                phẩm organic có chất lượng vượt trội khác biệt
+                                hẳn so với những dòng sản phẩm cùng loại trên thị
+                                trường
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-               <div class="col-md-6">
-                   <div class="info-3">
-                       <img style="height: 50px;" src="{{asset('img/icon-info-3.jpg')}}">
-                       <div  style="clear: both">
-                           <h4>Thực phẩm Organic</h4>
-                           <p>
-                               Luông mang trong mình tự hào với những thực
-                               phẩm organic có chất lượng vượt trội khác biệt
-                               hẳn so với những dòng sản phẩm cùng loại trên thị
-                               trường
-                           </p>
-                       </div>
-                   </div>
-               </div>
+                <div class="col-md-6">
+                    <div class="info-3">
+                        <img style="height: 50px;" src="{{asset('img/icon-info-3.jpg')}}">
+                        <div  style="clear: both">
+                            <h4>Thực phẩm Organic</h4>
+                            <p>
+                                Luông mang trong mình tự hào với những thực
+                                phẩm organic có chất lượng vượt trội khác biệt
+                                hẳn so với những dòng sản phẩm cùng loại trên thị
+                                trường
+                            </p>
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-6">
                     <div class="info-4 pull-right">
                         <img class="pull-right" style="height: 50px;" src="{{asset('img/icon-info-4.jpg')}}">
@@ -242,28 +242,28 @@
                         <div class="row col-md-12">
                             <?php $k = 1 ?>
                             @foreach($services as $service)
-                            @if($k == 1)
-                            <div class="col-md-6">
-                                <iframe width="500" id="iframe-video" height="315" src="{{$service->url_video}}"></iframe>
-                            </div>
-                            <div class="col-md-6">
-                                <h4>{{$service->name}}</h4>
-                                <p>
-                                    {{$service->description}}
-                                </p>
-                            </div>
-                            <div class="clearfix"></div>
-                            @else
-                            <div class="col-md-4">
-                                <img src="{{$service->image}}" alt="" style="width: 100%">
-                                <p>{{$service->name}}</p>
-                            </div>
-                            @endif
-                            <?php $k++; ?>
+                                @if($k == 1)
+                                    <div class="col-md-6">
+                                        <iframe width="500" id="iframe-video" height="315" src="{{$service->url_video}}"></iframe>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4>{{$service->name}}</h4>
+                                        <p>
+                                            {{$service->description}}
+                                        </p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                @else
+                                    <div class="col-md-4">
+                                        <img src="{{$service->image}}" alt="" style="width: 100%">
+                                        <p>{{$service->name}}</p>
+                                    </div>
+                                @endif
+                                <?php $k++; ?>
                             @endforeach
                         </div>
 
-                </div>
+                    </div>
                 </div>
             </div>
             <!-- /row -->
@@ -281,14 +281,44 @@
             <hr style="width: 100px; text-align: center; border: 1px solid">
 
             <div id="owl-demo" class=" owl-theme">
-                <div class="item"><img src="assets/img/clients/client-1.png" alt="Owl Image"></div>
-                <div class="item"><img src="assets/img/clients/client-3.png" alt="Owl Image"></div>
-                {{--<div class="item"><img src="assets/img/clients/client-4.png" alt="Owl Image"></div>--}}
-                <div class="item"><img src="assets/img/clients/client-2.png" alt="Owl Image"></div>
-                <div class="item"><img src="assets/img/clients/client-5.png" alt="Owl Image"></div>
-                <div class="item"><img src="assets/img/clients/client-6.png" alt="Owl Image"></div>
-                <div class="item"><img src="assets/img/clients/client-7.png" alt="Owl Image"></div>
-                <div class="item"><img src="assets/img/clients/client-8.png" alt="Owl Image"></div>
+                <div class="item">
+                    <a href="http://ameii.vn/">
+                        <img src="http://ameii.vn/data/banner/usl1553652990.png" alt="Owl Image">
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="https://www.mard.gov.vn/Pages/default.aspx">
+                        <img src="https://www.mard.gov.vn/PublishingImages/Footer/bnn-logo.png" alt="Owl Image">
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="http://gib.com.vn/">
+                        <img src="http://gib.com.vn/uploads/worigin/2017/05/25/Logo_gib-up_web592646c7b6156_850c2ef14d9158404479e263aab36f23.png" alt="Owl Image">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="https://nmri.org.vn/">
+                        <img src="https://nmri.org.vn/uploads/logo_2.png" alt="Owl Image">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="https://www.bigc.vn/">
+                        <img style="height: 151px;background-size: auto;" src="https://www.bigc.vn/img/logo.png?v=1" alt="Owl Image">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="http://www.japfavietnam.com/">
+                        <img src="http://www.japfavietnam.com/images/logo.png" alt="Owl Image">
+                    </a>
+                </div>
+                <div class="item"> <a href="https://www.pvcfc.com.vn/" ><img src="https://www.pvcfc.com.vn/Data/Sites/1/media/logo/logopvcfc.png" alt="Owl Image"></a></div>
+                <div class="item">
+                    <a href="https://www.hcmuaf.edu.vn/">
+                        <img src="https://www.hcmuaf.edu.vn/images/banner_nlu.jpg" alt="Owl Image">
+                    </a>
+                </div>
             </div>
 
             <style>
@@ -300,6 +330,7 @@
                     width: 80%;
                     height: auto;
                     margin: auto;
+                    background-size: cover;
                 }
                 #owl-demo .owl-stage-outer{
                     overflow: hidden;
