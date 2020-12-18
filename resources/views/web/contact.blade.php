3699349e231c38833cdf2bae331a04d230b28e8f
romecoder@gmail.com <tung12345>
2020-12-18 21:10:31 +0700

    sua trang liên hệ

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -5,16 +5,6 @@
         <div class="section">
             <!-- container -->
             <!-- row -->
-            {{--<div class="page-header">--}}
-                {{--<div class="container">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-md-offset-1 col-md-10 text-center">--}}
-                            {{--<h1 class="text-uppercase">Contacts</h1>--}}
-                            {{--<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
             <div class="container">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.1428071996233!2d105.79098221540187!3d20.98691189458037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acc87d153fe7%3A0x16232c8f03de132b!2zTDAzLCA4IE5nw7UgNjcgUGjDuW5nIEtob2FuZywgVHJ1bmcgVsSDbiwgVOG7qyBMacOqbSwgSMOgIE7hu5lpLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1607917026875!5m2!1sen!2s"
                         width="100%"
@@ -30,47 +20,21 @@
                             <div class="col-md-8">
                                 <div class="section-row">
                                     <div class="section-title">
-                                        <h2 class="title">Contact Information</h2>
+                                        <h2 class="title">Thông tin liên hệ</h2>
                                     </div>
-                                    <p>Malis debet quo et, eam an lorem quaestio. Mea ex quod facer decore, eu nam mazim postea. Eu deleniti pertinacia ius. Ad elitr latine eam, ius sanctus eleifend no, cu primis graecis comprehensam eum. Ne vim prompta consectetuer, etiam signiferumque ea eum.</p>
+                                    <p>VietFarm cung cấp tự hàng cung cấp những công nghệ phát triển nhất áp dụng vào cây trồng.</p>
                                     <ul class="contact">
                                         <li><i class="fa fa-phone"></i> 202-555-0194</li>
                                         <li><i class="fa fa-envelope"></i> <a href="#">callie@email.com</a></li>
                                         <li><i class="fa fa-map-marker"></i> 123 6th St.Melbourne, FL 32904</li>
                                     </ul>
                                 </div>
-
-                                {{--                        <div class="section-row">--}}
-                                {{--                            <div class="section-title">--}}
-                                {{--                                <h2 class="title">Mail us</h2>--}}
-                                {{--                            </div>--}}
-                                {{--                            <form>--}}
-                                {{--                                <div class="row">--}}
-                                {{--                                    <div class="col-md-12">--}}
-                                {{--                                        <div class="form-group">--}}
-                                {{--                                            <input class="input" type="email" name="email" placeholder="Email">--}}
-                                {{--                                        </div>--}}
-                                {{--                                    </div>--}}
-                                {{--                                    <div class="col-md-12">--}}
-                                {{--                                        <div class="form-group">--}}
-                                {{--                                            <input class="input" type="text" name="subject" placeholder="Subject">--}}
-                                {{--                                        </div>--}}
-                                {{--                                    </div>--}}
-                                {{--                                    <div class="col-md-12">--}}
-                                {{--                                        <div class="form-group">--}}
-                                {{--                                            <textarea class="input" name="message" placeholder="Message"></textarea>--}}
-                                {{--                                        </div>--}}
-                                {{--                                        <button class="primary-button">Submit</button>--}}
-                                {{--                                    </div>--}}
-                                {{--                                </div>--}}
-                                {{--                            </form>--}}
-                                {{--                        </div>--}}
                             </div>
                             <div class="col-md-4">
                                 <!-- social widget -->
                                 <div class="aside-widget">
                                     <div class="section-title">
-                                        <h2 class="title">Social Media</h2>
+                                        <h2 class="title">Truyền thông</h2>
                                     </div>
                                     <div class="social-widget">
                                         <ul>
