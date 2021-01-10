29ef985e36a93e66e0796be3db03b179df388418
romecoder@gmail.com <tung12345>
2021-01-10 13:14:57 +0700

    giao dien trang chu

--- resources/views/web/fertiizer-detail.blade.php
+++ resources/views/web/fertiizer-detail.blade.php
@@ -24,17 +24,6 @@
                             </ul>
                         </div>
                     </div>
-                    <!-- /category widget -->
-                    {{--<!-- galery widget -->--}}
-                    {{--<div class="aside-widget">--}}
-                        {{--<div class="section-title">--}}
-                            {{--<h2 class="title">Chứng Nhận</h2>--}}
-                        {{--</div>--}}
-                        {{--<div class="galery-widget">--}}
-                            {{--<img src="{{asset('./img/galery-1.jpg')}}" alt="" style="width: 250px">--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{--<!-- /galery widget -->--}}
                 </div>
                 <div class="col-md-9">
                     <div class="row">
@@ -42,69 +31,11 @@
                             <div class="wrapper">
                                 <h1 style="text-align: left">{{$fertilizers->name}}</h1>
                                 <div class="image-gallery">
-                                    {{--<aside class="thumbnails">--}}
-                                        {{--<a href="javascript:void(0)" class="selected thumbnail" data-big="{{$fertilizers->image}}">--}}
-                                            {{--<div class="thumbnail-image" style="background-image: url({{$fertilizers->image}})"></div>--}}
-                                        {{--</a>--}}
-                                        {{--<a href="javascript:void(0)" class="thumbnail" data-big="{{$fertilizers->image}}">--}}
-                                            {{--<div class="thumbnail-image" style="background-image: url({{$fertilizers->image}})"></div>--}}
-                                        {{--</a>--}}
-                                        {{--<a href="javascript:void(0)" class="thumbnail" data-big="{{$fertilizers->image}}">--}}
-                                            {{--<div class="thumbnail-image" style="background-image: url({{$fertilizers->image}})"></div>--}}
-                                        {{--</a>--}}
-                                    {{--</aside>--}}
-
                                     <main class="primary" style="background-image: url({{$fertilizers->image}});"></main>
                                 </div>
 
                             </div>
                         </div>
-                        {{--<div class="col-md-4">--}}
-                            {{--<div class="property-agent">--}}
-                                {{--<h4 class="title-agent">Anabella Geller</h4>--}}
-                                {{--<p class="color-text-a">--}}
-                                    {{--Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet--}}
-                                    {{--dui. Quisque velit nisi,--}}
-                                    {{--pretium ut lacinia in, elementum id enim.--}}
-                                {{--</p>--}}
-                                {{--<ul class="list">--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Property ID:</strong>--}}
-                                        {{--<span>1134</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Location:</strong>--}}
-                                        {{--<span>Chicago, IL 606543</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Property Type:</strong>--}}
-                                        {{--<span>House</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Status:</strong>--}}
-                                        {{--<span>Sale</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Area:</strong>--}}
-                                        {{--<span>340m--}}
-                          {{--<sup>2</sup>--}}
-                        {{--</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Beds:</strong>--}}
-                                        {{--<span>4</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Baths:</strong>--}}
-                                        {{--<span>2</span>--}}
-                                    {{--</li>--}}
-                                    {{--<li class="d-flex justify-content-between">--}}
-                                        {{--<strong>Garage:</strong>--}}
-                                        {{--<span>1</span>--}}
-                                    {{--</li>--}}
-                                {{--</ul>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
                     </div>
                     <div class="row">
                         <div class="col-md-12">
@@ -162,9 +93,6 @@
                                         <a href="{{url('/san-pham', $lq->url)}}">{{$lq->name}}</a>
                                     </h2>
                                 </div>
-                                {{--<div class="card-date">--}}
-                                    {{--<span class="date-b">3.450.000 đ</span>--}}
-                                {{--</div>--}}
                             </div>
                         </div>
                     </div>
